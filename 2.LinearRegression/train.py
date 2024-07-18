ed155122ba50ed5dfa99c5a14d7545c66ba56651
David Mg Mg <125610886+DepressoLofi@users.noreply.github.com>
2024-07-18 14:16:51 +0630

    logistic regression

--- 2.LinearRegression/train.py
+++ 2.LinearRegression/train.py
@@ -8,9 +8,9 @@ from LinearRegression import LinearRegression
 X, y = datasets.make_regression(n_samples=100, n_features=1, noise=20, random_state=4)
 X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=1234)
 
-# fig=plt.figure(figsize=(8,6))
-# plt.scatter(X[:, 0], y, color="b", marker="o", s = 30)
-# plt.show()
+fig=plt.figure(figsize=(8,6))
+plt.scatter(X[:, 0], y, color="b", marker="o", s = 30)
+plt.show()
 
 
 reg = LinearRegression(lr=0.01)
