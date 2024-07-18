97f83d0e63215608eebc117d49866571f251319f
David Mg Mg <125610886+DepressoLofi@users.noreply.github.com>
2024-07-18 15:43:33 +0630

    decision tree

---
+++ 4.DecisionTrees/DecisionTree.py
@@ -0,0 +1,28 @@
+
+
+
+class Node:
+
+    def __init__(self, feature=None, threshold=None, left=None, right=None, *, value=None):
+        self.feature = feature
+        self.threshold = threshold
+        self.left = left
+        self.right = right
+        self.value = None
+
+    def is_leaf_node(self):
+        return self.value is not None
+
+
+class DecisionTree:
+
+    def __init__(self, min_samples_split=2, max_depth=100, n_features=None):
+        self.min_samples_split=min_samples_split
+        self.max_depth=max_depth
+        self.n_features=n_features
+        self.root=None
+
+    def fit():
+
+
+    def predict():
